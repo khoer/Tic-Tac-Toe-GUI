4249df36ea875b3a9d52f44396239953f10323e6
Khoerudin <khoe.tw@gmail.com>
2015-07-29 11:50:42 +0700

    Create ResetButton.java

---
+++ src/creation/sahif/tictactoegui/ResetButton.java
@@ -0,0 +1,14 @@
+package creation.sahif.tictactoegui;
+
+import javax.swing.JButton;
+
+/*
+ *create ResetButton inherit JButton
+ *@author Khoerudin
+ */
+
+public class ResetButton extends JButton{
+	public ResetButton(){
+		setText("RESET");
+	}
+}
