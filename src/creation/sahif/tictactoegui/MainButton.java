dbefbd5fe1d0ce576cbd079068ddadff87d69466
Khoerudin <khoe.tw@gmail.com>
2015-07-29 11:49:58 +0700

    Create MainButton.java

---
+++ src/creation/sahif/tictactoegui/MainButton.java
@@ -0,0 +1,13 @@
+package creation.sahif.tictactoegui;
+
+import javax.swing.JButton;
+
+/*
+ *create Main Button inherit JButton
+ *@author Khoerudin
+ */
+
+public class MainButton extends JButton{       
+    public MainButton(){
+    }
+}
